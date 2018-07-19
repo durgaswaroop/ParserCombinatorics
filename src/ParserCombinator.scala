d0dd442d0842797c32e8bb945a270cffeb19bbff
durgaswaroop <durgaswaroop@gmail.com>
2018-07-20 03:54:46 +0530

    Ensure that return type of 'andThen' of two parsers is Either[String, ((T, T), String)]
    If we don't ensure this it becomes EIther[String, (Any, String)] which is not what we want. So, explicitly ensuring this will make sure we don't have any issues later.

--- src/ParserCombinator.scala
+++ src/ParserCombinator.scala
@@ -1,15 +1,16 @@
 object ParserCombinator {
-  def andThen[T](parser1: Parser[T], parser2: Parser[T])(input: String) = {
+  def andThen[T](parser1: Parser[T], parser2: Parser[T])(
+      input: String): Either[String, ((T, T), String)] = {
 
     val firstParse = parser1.func(input)
 
     firstParse match {
-      case Left(_) => firstParse
+      case Left(value1) => Left(value1)
       case Right((matched1, remaining1)) =>
         val secondParse = parser2.func(remaining1)
 
         secondParse match {
-          case Left(_) => secondParse
+          case Left(value2) => Left(value2)
           case Right((matched2, remaining2)) =>
             Right((matched1, matched2), remaining2)
         }
