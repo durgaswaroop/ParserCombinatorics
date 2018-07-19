0525cc7d2040d308754a60885971c7e8bdd3e7fc
durgaswaroop <durgaswaroop@gmail.com>
2018-07-20 03:52:58 +0530

    Refactor tests

--- test/BasicParserSpec.scala
+++ test/BasicParserSpec.scala
@@ -3,20 +3,24 @@ import BasicParser._
 class BasicParserSpec extends TestBase("BasicParser") {
 
   it should "return a Either object for the input" in {
-    parseChar('a')("abc") shouldBe an[Either[_, _]]
+    val pchara = parseChar('a') _
+    pchara("abc") shouldBe an[Either[String, (String, Char)]]
   }
 
   it should
     "return remaining input & the matched character if the char is matched" in {
-    parseChar('A')("A").merge shouldBe ("", 'A')
+    val pcharA = parseChar('A') _
+    pcharA("A").merge shouldBe ("", 'A')
 
   }
 
   it should "return a failure msg in case of no match" in {
-    parseChar('C')("AB").merge shouldBe "Expecting 'C'. Got 'A'"
+    val pcharC = parseChar('C') _
+    pcharC("AB").merge shouldBe "Expecting 'C'. Got 'A'"
   }
 
   it should "return No more input for empty and null string inputs" in {
+    val pchar = parseChar('H') _
     parseChar('H')("").merge shouldBe "No more input"
     parseChar('H')(null).merge shouldBe "No more input"
   }
@@ -25,6 +29,7 @@ class BasicParserSpec extends TestBase("BasicParser") {
     val parseH = Parser(parseChar('H'))
     runParser("Hello", parseH).merge shouldBe ("ello", 'H')
     runParser("hola", parseH).merge shouldBe "Expecting 'H'. Got 'h'"
+    runParser("", parseH).merge shouldBe "No more input"
   }
 
 }
