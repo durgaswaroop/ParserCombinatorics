6d6bada7f3ed809c7c400f5cb12080c539ce2a32
durgaswaroop <durgaswaroop@gmail.com>
2018-07-20 03:54:49 +0530

    Make andThen of two parsers return another Parser
    andThen is pretty much like parser1 + parser2. So, the output should also be a parser. So, created a innerfunction inside the andthen method that gets put inside a parser object.

--- src/ParserCombinator.scala
+++ src/ParserCombinator.scala
@@ -1,22 +1,34 @@
+import BasicParser._
+
 object ParserCombinator {
-  def andThen[T](parser1: Parser[T], parser2: Parser[T])(
-      input: String): Either[String, ((T, T), String)] = {
 
-    val firstParse = parser1.func(input)
+  /**
+    * 1. Run the first parser on the given input.
+    *   - If there is a failure, just return from there.
+    * 2. If the first parsing succeeds, run the second parser on the remaining
+    * input
+    *   - If there is a failure, just return from here too.
+    * 3. If both the parsers succeed, return a tuple of both the parser values.
+    */
+  def andThen[T](parser1: Parser[T], parser2: Parser[T]): Parser[(T, T)] = {
 
-    firstParse match {
-      case Left(value1) => Left(value1)
-      case Right((matched1, remaining1)) =>
-        val secondParse = parser2.func(remaining1)
+    def innerFunc(input: String) = {
+      val firstParse = runParser(input, parser1)
 
-        secondParse match {
-          case Left(value2) => Left(value2)
-          case Right((matched2, remaining2)) =>
-            Right((matched1, matched2), remaining2)
-        }
+      firstParse match {
+        case Left(value1) => Left(value1)
+        case Right((matched1, remaining1)) =>
+          val secondParse = runParser(remaining1, parser2)
 
+          secondParse match {
+            case Left(value2) => Left(value2)
+            case Right((matched2, remaining2)) =>
+              Right((matched1, matched2), remaining2)
+          }
+      }
     }
 
+    Parser(innerFunc)
   }
 
 }
--- test/AndThenCombinatorSpec.scala
+++ test/AndThenCombinatorSpec.scala
@@ -5,20 +5,26 @@ class AndThenCombinatorSpec extends TestBase("AndThenCombinator") {
 
   val parserA: Parser[Char] = Parser(parseChar('A'))
   val parserB: Parser[Char] = Parser(parseChar('B'))
-  val andThenCombined = andThen(parserA, parserB) _
+  val andThenCombined: Parser[(Char, Char)] = andThen(parserA, parserB)
 
   it should "return output of parser2 when parser1 is successful" in {
-    andThenCombined("ABCD") shouldBe Right(('A', 'B'), "CD")
+    runParser("ABCD", andThenCombined) shouldBe Right(('A', 'B'), "CD")
   }
 
   it should "return failure when parser1 fails" in {
-    andThenCombined("BCD") shouldBe Left("Expecting 'A'. Got 'B'")
+    runParser("BCD", andThenCombined) shouldBe Left("Expecting 'A'. Got 'B'")
   }
 
   it should "return failure when parser2 fails" in {
-    andThenCombined("ACD") shouldBe Left("Expecting 'B'. Got 'C'")
+    runParser("ACD", andThenCombined) shouldBe Left("Expecting 'B'. Got 'C'")
   }
 
-  it should "return No more input when input is empty" in {}
+  it should "return No more input when input is empty" in {
+    runParser("", andThenCombined) shouldBe Left("No more input")
+  }
+
+  it should "return failure when the input to second parser is empty" in {
+    runParser("A", andThenCombined) shouldBe Left("No more input")
+  }
 
 }
