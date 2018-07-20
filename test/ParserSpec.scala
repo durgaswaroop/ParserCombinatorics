1c2ea222620b40289fcbb54c5c181975c6952e37
durgaswaroop <durgaswaroop@gmail.com>
2018-07-20 18:51:52 +0530

    Delete ParserCombinator class
    Rename ParserCombinatorSpec to ParserSpec.

---
+++ test/ParserSpec.scala
@@ -0,0 +1,42 @@
+import BasicParser._
+
+class ParserSpec extends TestBase("Parser") {
+  val parserA = Parser(parseChar('A'))
+  val parserB = Parser(parseChar('B'))
+  val parserC = Parser(parseChar('C'))
+
+  val parser1 = Parser(parseChar('1'))
+  val parser2 = Parser(parseChar('2'))
+
+  it should "parse correctly when I combine orElse with andThen" in {
+    val aAndThenBorC = parserA >> (parserB <|> parserC)
+
+    runParser("ABZ", aAndThenBorC) shouldBe Right(('A', 'B'), "Z")
+    runParser("ACZ", aAndThenBorC) shouldBe Right(('A', 'C'), "Z")
+  }
+
+  it should "parse correctly when I combine orElse with map" in {
+    val oneOrElseTwo = parser1 <|> parser2
+    val asInt = (c: Char) => c.asDigit
+    val aOrElseBAsInt = oneOrElseTwo |>> asInt
+
+    runParser("123", aOrElseBAsInt) shouldBe Right(1, "23")
+  }
+
+  it should "parse correctly when I combine andThen with map" in {
+    //noinspection ScalaUnnecessaryParentheses
+    val asString: ((Char, Char)) => String = t => t._1.toString + t._2.toString
+    val aThenBAsString = parserA >> parserB |>> asString
+
+    runParser("ABC", aThenBAsString) shouldBe Right("AB", "C")
+
+    val oneThenTwoAsString = parser1 >> parser2 |>> asString
+
+    runParser("12C", oneThenTwoAsString) shouldBe Right("12", "C")
+
+    val asInt: String => Int = s => s.toInt
+    val oneThenTwoAsStringAsInt = parser1 >> parser2 |>> asString |>> asInt
+
+    runParser("12C", oneThenTwoAsStringAsInt) shouldBe Right(12, "C")
+  }
+}
